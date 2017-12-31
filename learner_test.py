2874ef6f338a352ead16f8d7b5d0ef9ed0f30a00
tsokk <tso@sci.fi>
2017-12-31 13:15:05 +0200

    Correct imports to reflect filename changes

--- learner_test.py
+++ learner_test.py
@@ -1,26 +1,24 @@
 import numpy as np
 import unittest
-import bin
-
-from io import BytesIO
+import learner
 
 class TestBinFunctions(unittest.TestCase):
 
     def test_dist(self):
         x1 = np.array([2, 2])
         x2 = np.array([3, 6])
-        result = bin.dist(x1, x2)
+        result = learner.dist(x1, x2)
         expected = 4.1231
         diff = result - expected
         self.assertTrue(abs(diff) < 0.0001)
 
     def test_read_data(self):
-        xs = bin.read_data("xs.txt")
+        xs = learner.read_data("xs.txt")
         self.assertEquals(xs.shape, (8, 2))
 
     def test_compute_distances(self):
-        xs = bin.read_data("xs.txt")
-        dists = bin.compute_distances(xs)
+        xs = learner.read_data("xs.txt")
+        dists = learner.compute_distances(xs)
         self.assertEquals(dists.shape, (8, 8))
 
 if __name__ == '__main__':
