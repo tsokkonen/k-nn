4133428241ce7fe905d5efb9e918a9c9aa72d257
tsokkonen <tsokkonen@gmail.com>
2017-12-31 17:52:31 +0200

    Simplified code

--- learner.py
+++ learner.py
@@ -1,19 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def dist(a, b):
-    return np.linalg.norm(a - b)
+def dist(x, xs):
+       diffs = xs - x
+       f = np.linalg.norm
+       return np.apply_along_axis(f, 1, diffs)
+
+def one_nn(x, xs, ys):
+       d = dist(x, xs)
+       return ys[d.argmin()]
 
 def read_data(fname):
     return np.genfromtxt(fname, delimiter=",")
     
-def compute_distances(xs):
-    dists = []
-    for i in range(len(xs)):
-        for x in xs:
-            dists.append(dist(xs[i], x))
-    return np.reshape(dists, (len(xs), len(xs)))
-
 def show_samples(xs, ys):
     plt.scatter(xs[:,0], xs[:,1], c=ys, cmap='Greys')
     return plt.show()
--- learner_test.py
+++ learner_test.py
@@ -5,21 +5,25 @@ import learner
 class TestLearnerFunctions(unittest.TestCase):
 
     def test_dist(self):
-        x1 = np.array([2, 2])
-        x2 = np.array([3, 6])
-        result = learner.dist(x1, x2)
+        x = np.array([2, 2])
+        xs = learner.read_data("xs.txt")
+        result = learner.dist(x, xs)
         expected = 4.1231
-        diff = result - expected
+        diff = result[1] - expected
         self.assertTrue(abs(diff) < 0.0001)
 
     def test_read_data(self):
         xs = learner.read_data("xs.txt")
         self.assertEquals(xs.shape, (8, 2))
 
-    def test_compute_distances(self):
+    def test_one_nn(self):
         xs = learner.read_data("xs.txt")
-        dists = learner.compute_distances(xs)
-        self.assertEquals(dists.shape, (8, 8))
+        ys = learner.read_data("ys.txt")
+        x = np.array([9, 9])
+        result = learner.one_nn(x, xs, ys)
+        expected = 1.0
+        diff = result - expected
+        self.assertTrue(abs(diff) < 0.0001)
 
 if __name__ == '__main__':
     unittest.main()
